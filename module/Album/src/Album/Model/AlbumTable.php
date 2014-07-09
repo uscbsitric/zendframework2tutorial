0b9fbebcdc000156a3d7fa5a91d72e5a1e5686e5
uscbsitric <usc_bsit_ric@yahoo.com>
2014-07-10 00:37:22 +0800

    initial commit

---
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -0,0 +1,79 @@
+<?php
+namespace Album\Model;
+
+ // Add these import statements:
+ use Album\Model\Album;
+ use Album\Model\AlbumTable;
+ use Zend\Db\ResultSet\ResultSet;
+ use Zend\Db\TableGateway\TableGateway;
+ 
+
+ class AlbumTable
+ {
+     protected $tableGateway;
+
+     public function __construct(TableGateway $tableGateway)
+     {
+         $this->tableGateway = $tableGateway;
+     }
+     // Add this method:
+     public function getServiceConfig()
+     {
+     	return array(
+     			'factories' => array(
+     					'Album\Model\AlbumTable' =>  function($sm) {
+     						$tableGateway = $sm->get('AlbumTableGateway');
+     						$table = new AlbumTable($tableGateway);
+     						return $table;
+     					},
+     					'AlbumTableGateway' => function ($sm) {
+     						$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+     						$resultSetPrototype = new ResultSet();
+     						$resultSetPrototype->setArrayObjectPrototype(new Album());
+     						return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+     					},
+     			),
+     	);
+     }
+      
+     public function fetchAll()
+     {
+         $resultSet = $this->tableGateway->select();
+         return $resultSet;
+     }
+
+     public function getAlbum($id)
+     {
+         $id  = (int) $id;
+         $rowset = $this->tableGateway->select(array('id' => $id));
+         $row = $rowset->current();
+         if (!$row) {
+             throw new \Exception("Could not find row $id");
+         }
+         return $row;
+     }
+
+     public function saveAlbum(Album $album)
+     {
+         $data = array(
+             'artist' => $album->artist,
+             'title'  => $album->title,
+         );
+
+         $id = (int) $album->id;
+         if ($id == 0) {
+             $this->tableGateway->insert($data);
+         } else {
+             if ($this->getAlbum($id)) {
+                 $this->tableGateway->update($data, array('id' => $id));
+             } else {
+                 throw new \Exception('Album id does not exist');
+             }
+         }
+     }
+
+     public function deleteAlbum($id)
+     {
+         $this->tableGateway->delete(array('id' => (int) $id));
+     }
+ }
